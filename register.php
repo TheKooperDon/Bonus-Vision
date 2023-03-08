6a236551d0c6616b0f39b761526aedc721b00dc0
Max24p7 <82169517+Max24p7@users.noreply.github.com>
2023-03-08 13:39:23 -0600

    Update register.php

--- register.php
+++ register.php
@@ -10,19 +10,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	require ('mysqli_connect.php'); // Connect to the db.
 		
 	$errors = array(); // Initialize an error array.
-	
+
+	// Check for a user name:
+	if (empty($_POST['username'])) {
+		$errors[] = 'You forgot to enter a username.';
+	} else {
+		$fn = mysqli_real_escape_string($dbc, trim($_POST['username']));
+	}
+
 	// Check for a first name:
-	if (empty($_POST['first_name'])) {
+	if (empty($_POST['firstname'])) {
 		$errors[] = 'You forgot to enter your first name.';
 	} else {
-		$fn = mysqli_real_escape_string($dbc, trim($_POST['first_name']));
+		$fn = mysqli_real_escape_string($dbc, trim($_POST['firstname']));
 	}
 	
 	// Check for a last name:
-	if (empty($_POST['last_name'])) {
+	if (empty($_POST['lastname'])) {
 		$errors[] = 'You forgot to enter your last name.';
 	} else {
-		$ln = mysqli_real_escape_string($dbc, trim($_POST['last_name']));
+		$ln = mysqli_real_escape_string($dbc, trim($_POST['lastname']));
 	}
 	
 	// Check for an email address:
@@ -33,11 +40,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	}
 	
 	// Check for a password and match against the confirmed password:
-	if (!empty($_POST['pass1'])) {
-		if ($_POST['pass1'] != $_POST['pass2']) {
+	if (!empty($_POST['password1'])) {
+		if ($_POST['password1'] != $_POST['password2']) {
 			$errors[] = 'Your password did not match the confirmed password.';
 		} else {
-			$p = mysqli_real_escape_string($dbc, trim($_POST['pass1']));
+			$p = mysqli_real_escape_string($dbc, trim($_POST['password1']));
 		}
 	} else {
 		$errors[] = 'You forgot to enter your password.';
@@ -48,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		// Register the user in the database...
 		
 		// Make the query:
-		$q = "INSERT INTO users (first_name, last_name, email, pass, registration_date) VALUES ('$fn', '$ln', '$e', SHA2('$p',256), NOW() )";		
+		$q = "INSERT INTO users (username, firstname, lastname, email, password) VALUES ('$fn', '$ln', '$e', SHA2('$p',256), NOW() )";		
 		$r = @mysqli_query ($dbc, $q); // Run the query.
 		if ($r) { // If it ran OK.
 		
