0306bf40a7d4e6cb84df98e3344b6e8d4ee26d54
Max24p7 <82169517+Max24p7@users.noreply.github.com>
2023-03-08 15:34:16 -0600

    Update register.php

--- register.php
+++ register.php
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		
 			// Print a message:
 			echo '<h1>Thank you!</h1>
-		<p>You are now registered. In Chapter 12 you will actually be able to log in!</p><p><br /></p>';	
+		<p>You are now registered.</p><p><br /></p>';	
 		
 		} else { // If it did not run OK.
 			
@@ -97,11 +97,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 ?>
 <h1>Register</h1>
 <form action="register.php" method="post">
-	<p>First Name: <input type="text" name="first_name" size="15" maxlength="20" value="<?php if (isset($_POST['first_name'])) echo $_POST['first_name']; ?>" /></p>
-	<p>Last Name: <input type="text" name="last_name" size="15" maxlength="40" value="<?php if (isset($_POST['last_name'])) echo $_POST['last_name']; ?>" /></p>
+	<p>UserName: <input type="text" name="username" size="15" maxlength="20" value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>" /></p>
+	<p>First Name: <input type="text" name="firstname" size="15" maxlength="20" value="<?php if (isset($_POST['firstname'])) echo $_POST['firstname']; ?>" /></p>
+	<p>Last Name: <input type="text" name="lastname" size="15" maxlength="40" value="<?php if (isset($_POST['lastname'])) echo $_POST['lastname']; ?>" /></p>
 	<p>Email Address: <input type="text" name="email" size="20" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>"  /> </p>
-	<p>Password: <input type="password" name="pass1" size="10" maxlength="20" value="<?php if (isset($_POST['pass1'])) echo $_POST['pass1']; ?>"  /></p>
-	<p>Confirm Password: <input type="password" name="pass2" size="10" maxlength="20" value="<?php if (isset($_POST['pass2'])) echo $_POST['pass2']; ?>"  /></p>
+	<p>Password: <input type="password" name="password1" size="10" maxlength="20" value="<?php if (isset($_POST['password1'])) echo $_POST['password1']; ?>"  /></p>
+	<p>Confirm Password: <input type="password" name="password2" size="10" maxlength="20" value="<?php if (isset($_POST['password2'])) echo $_POST['password2']; ?>"  /></p>
 	<p><input type="submit" name="submit" value="Register" /></p>
 </form>
 <?php include ('footer.php'); ?>
