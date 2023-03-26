4c23e9f1601d6fb49abb5c4259d3482cff01fba8
Max24p7 <82169517+Max24p7@users.noreply.github.com>
2023-03-26 12:36:34 -0500

    Footer Added

--- footer.php
+++ footer.php
@@ -1,2 +1,51 @@
+<style>
+.facebook-icon {
+        color: #3B5998;
+        font-size: 1.5em;
+    }
+
+    .facebook-icon:hover {
+        opacity: 0.7;
+    }
+
+    .twitter-icon {
+        color: #00acee;
+        font-size: 1.5em;
+    }
+
+    .twitter-icon:hover {
+        opacity: 0.7;
+    }
+
+    .insta-icon {
+        color: #8a3ab9;
+        font-size: 1.5em;
+    }
+
+    .insta-icon:hover {
+        opacity: 0.7;
+    }
+  </style>
+
+<footer class="bg-dark text-white py-1 footer-bg">
+          <div class="container text-center">
+            <div class="row">
+              <div class="col">
+                <img src="/logoimages/whitelogoonly.svg" alt="Bootstrap" width="100" height="24">
+                <img src="/logoimages/whitetextonly1.svg" alt="Bootstrap" width="200" height="24">
+              </div>
+              <div class="col-5">
+            <a href="#"><i class="bi bi-instagram mx-2 insta-icon"></i></a>
+            <a href="#"><i class="bi bi-facebook mx-2 facebook-icon"></i></a>
+            <a href="#"><i class="bi bi-twitter mx-2 twitter-icon"></i></a>
+              </div>
+              <div class="col">
+                <p><a href="#">Copyright and Licensing</a></p>
+              </div>
+            </div>
+          </div>
+        
+    </footer>
+
 </body>
 </html>
