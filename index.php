5f34a64177b3f680942bf0dfb5fb0b631c46dc20
TheKooperDon <valodinejr@gmail.com>
2023-03-27 14:03:06 -0500

    1

--- index.php
+++ index.php
@@ -255,7 +255,7 @@ echo "</div>";
         <h2>Bonus Features</h2>
     </div>
     <div class="container">
-      <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="row row-cols-1 row-cols-md-1 g-4">
         <div class="col">
           <div class="card bg-dark text-center">
             <h5 class="card-header text-white">Deleted Scenes</h5>
