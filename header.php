240bfeba2e159b9dcb36845cf0dbe056e5855ec4
Max24p7 <82169517+Max24p7@users.noreply.github.com>
2023-03-29 13:43:46 -0500

    Update header.php

--- header.php
+++ header.php
@@ -19,6 +19,7 @@ if ( isset($_SESSION['user_id']) && ($_SESSION['user_id'] == 6) ) {
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.typekit.net/fqq5ksl.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 </head>
 <body>
 <header>
