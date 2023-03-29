050261b3a3b69b4ad612c0142a21be81587920ff
Max24p7 <82169517+Max24p7@users.noreply.github.com>
2023-03-29 13:02:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -212,10 +212,10 @@ echo "</div>";
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">About Us</a>
+                <a class="nav-link" href="aboutus.html">About Us</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Movies</a>
+                <a class="nav-link" href="movies.html">Movies</a>
               </li>
               
               <li class="nav-item dropdown">
