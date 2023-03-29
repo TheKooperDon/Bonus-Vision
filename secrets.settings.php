71641fa184ebb79fed3f647020818901c0f43558
Matt <8096633+fvrlo@users.noreply.github.com>
2023-03-29 17:47:20 -0500

    Delete secrets.settings.php

--- secrets.settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-DEFINE ('DB_USER', 'infost490s3_admin');
-DEFINE ('DB_PASSWORD', 'KfY&DFVd;#CM');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'infost490s3_bonus');
-DEFINE ('TMDB_API_KEY', 'e9a6655bae34bf694a0f3e33338dc28e');
-?>
