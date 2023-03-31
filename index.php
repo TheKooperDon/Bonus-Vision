6f140911ad8690a3b531f418013893893599862c
TheKooperDon <valodinejr@gmail.com>
2023-03-31 12:58:01 -0500

    size change added car 2

--- index.php
+++ index.php
@@ -129,13 +129,13 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="moviedvdcovers/shrek2.jpg" class="d-block w-100" alt="...">
+      <img src="moviedvdcovers/shrek2.jpg" class="d-block w-30" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="moviedvdcovers/shrek2.jpg" class="d-block w-100" alt="...">
+      <img src="moviedvdcovers/shrek2.jpg" class="d-block w-30" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="moviedvdcovers/shrek2.jpg" class="d-block w-100" alt="...">
+      <img src="moviedvdcovers/shrek2.jpg" class="d-block w-30" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -147,6 +147,32 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+
+
+<!-- Number 2 carsouel with a card -->
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="moviedvdcovers/shrek2.jpg" class="d-block w-30" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="moviedvdcovers/shrek2.jpg" class="d-block w-30" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="moviedvdcovers/shrek2.jpg" class="d-block w-30" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
 
 
 <!-- Carousel  -->
