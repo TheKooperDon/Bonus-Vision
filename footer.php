1c6487d42bf5fdf11251cc37d1dcef72889bc4d0
Max24p7 <82169517+Max24p7@users.noreply.github.com>
2023-03-31 13:00:47 -0500

    force footer to ignore body padding

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-dark text-white py-1 footer-bg mt-auto">
+<footer id="footer" class="bg-dark text-white py-1 footer-bg mt-auto">
   <div class="container text-center">
     <div class="row">
       <div class="col">
