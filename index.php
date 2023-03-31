33e30d1103b08a7d80f3c55ea69b0247fb0a0310
TheKooperDon <valodinejr@gmail.com>
2023-03-31 13:16:12 -0500

    making room for this to work

--- index.php
+++ index.php
@@ -183,27 +183,7 @@
 
       <!-- Number 2 carsouel with a card -->
 
-      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="moviedvdcovers/shrek2.jpg" class="d-inline w-30" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="moviedvdcovers/shrek2.jpg" class="d-inline w-30" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="moviedvdcovers/shrek2.jpg" class="d-inline w-30" alt="...">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
+      
 
 
 
