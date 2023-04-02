57df89cbd5afe5c3082a3d3d8d7ee1c28ec2c3e6
Matt <8096633+fvrlo@users.noreply.github.com>
2023-04-02 11:48:17 -0500

    wdweadadeeasf

--- login.php
+++ login.php
@@ -102,8 +102,72 @@ if (($_SERVER['REQUEST_METHOD'] == 'POST') && ($_SERVER['QUERY_STRING'] == 'regi
 		}
 		echo '</p><p>Please try again.</p><p><br /></p>';
 	} // End of if (empty($errors)) IF.
+	mysqli_close($dbc); // Close the database connection.
 }
 echo ($_SERVER['QUERY_STRING'] == "error") ? "<h2>Error, login failed</h2>" : "";
+
+
+
+if (($_SERVER['REQUEST_METHOD'] == 'POST') && ($_SERVER['QUERY_STRING'] == 'changepass')){
+	$errors = array(); // Initialize an error array.
+	if (empty($_POST['email'])) {
+		$errors[] = 'You forgot to enter your email address.';
+	} else {
+		$e = mysqli_real_escape_string($dbc, trim($_POST['email']));
+	}
+	if (empty($_POST['password'])) {
+		$errors[] = 'You forgot to enter your current password.';
+	} else {
+		$p = mysqli_real_escape_string($dbc, trim($_POST['password']));
+	}
+	if (!empty($_POST['password1'])) {
+		if ($_POST['password1'] != $_POST['password2']) {
+			$errors[] = 'Your new password did not match the confirmed password.';
+		} else {
+			$np = mysqli_real_escape_string($dbc, trim($_POST['password1']));
+		}
+	} else {
+		$errors[] = 'You forgot to enter your new password.';
+	}
+	
+	if (empty($errors)) { // If everything's OK.
+		$q = "SELECT user_id FROM users WHERE (email='$e' AND password=SHA2('$p',256) )";
+		$r = @mysqli_query($dbc, $q);
+		$num = @mysqli_num_rows($r);
+		if ($num == 1) { // Match was made.
+			$row = mysqli_fetch_array($r, MYSQLI_NUM);
+			$q = "UPDATE users SET password=SHA2('$np',256) WHERE user_id=$row[0]";		
+			$r = @mysqli_query($dbc, $q);
+			if (mysqli_affected_rows($dbc) == 1) { // If it ran OK.
+				echo '<h1>Thank you!</h1>
+				<p>Your password has been updated successfully.</p><p><br /></p>';	
+			} else { // If it did not run OK.
+				echo '<h1>System Error</h1>
+				<p class="error">Your password could not be changed due to a system error. We apologize for any inconvenience.</p>'; 
+				echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $q . '</p>';
+			}
+			mysqli_close($dbc); // Close the database connection.
+			include ('footer.php'); 
+			exit();
+		} else { // Invalid email address/password combination.
+			echo '<h1>Error!</h1>
+			<p class="error">The email address and password do not match those on file.</p>';
+		}
+	} else { // Report the errors.
+		echo '<h1>Error!</h1>
+		<p class="error">The following error(s) occurred:<br />';
+		foreach ($errors as $msg) { // Print each error.
+			echo " - $msg<br />\n";
+		}
+		echo '</p><p>Please try again.</p><p><br /></p>';
+	} // End of if (empty($errors)) IF.
+	mysqli_close($dbc); // Close the database connection.
+} // End of the main Submit conditional.
+
+
+
+
+
 ?>
 <p>Login Status: <?php echo $is_logged_in ? "True" : "False"; ?></p>
 
@@ -128,8 +192,16 @@ if (!isset($_SESSION['agent']) OR ($_SESSION['agent'] != md5($_SERVER['HTTP_USER
 	echo '</form>';
 } else {
 	echo '<p>You are logged in as ', $_SESSION['username'], '</p>
-	<p><a href="accountsettings.php">Account Settings</a></p><br>
 	<p><a href="login.php?logout">Logout</a></p>
+	</br>
+	<h1>Change Your Password</h1>
+	<form action="login.php?changepass" method="post">
+		<p>Email Address: <input type="text" name="email" size="20" maxlength="60" value="', $_POST['email'], '"  /> </p>
+		<p>Current Password: <input type="password" name="password" size="10" maxlength="20" value="', $_POST['password'], '"  /></p>
+		<p>New Password: <input type="password" name="password1" size="10" maxlength="20" value="', $_POST['password1'], '"  /></p>
+		<p>Confirm New Password: <input type="password" name="password2" size="10" maxlength="20" value="', $_POST['password2'], '"  /></p>
+		<p><input type="submit" name="submit" value="Change Password" /></p>
+	</form>
 	';
 }
 ?>
