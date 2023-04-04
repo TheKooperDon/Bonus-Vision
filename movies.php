b340a8c35e6660e4729d747ff3816f7633bca635
TheKooperDon <valodinejr@gmail.com>
2023-04-04 10:18:17 -0500

    fixed cards

--- movies.php
+++ movies.php
@@ -67,6 +67,7 @@
           <div class="collapse" id="collapseExample">
             <div class="card card-body">
             <ul class="list-group list-group-flush">
+          
               <li class="list-group-item">A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case, far exceeding what anyone imagined he could do. But despite all he has achieved, his one true love eludes him.</li>
               <li class="list-group-item">Drama / Adventure</li>
               <li class="list-group-item">1994</li>
@@ -90,9 +91,10 @@
           </p>
           <div class="collapse" id="collapseExample">
             <div class="card card-body">
-              <p class="card-text">Nemo, an adventurous young clownfish, is unexpectedly taken from his Great Barrier Reef home to a dentist's office aquarium. It's up to his worrisome father Marlin and a friendly but forgetful fish Dory to bring Nemo home -- meeting vegetarian sharks, surfer dude turtles, hypnotic jellyfish, hungry seagulls, and more along the way.</p>
+              
           
             <ul class="list-group list-group-flush">
+              <li class="list-group-item">Nemo, an adventurous young clownfish, is unexpectedly taken from his Great Barrier Reef home to a dentist's office aquarium. It's up to his worrisome father Marlin and a friendly but forgetful fish Dory to bring Nemo home -- meeting vegetarian sharks, surfer dude turtles, hypnotic jellyfish, hungry seagulls, and more along the way.</li>
               <li class="list-group-item">Animation / Family</li>
               <li class="list-group-item">2003</li>
               <li class="list-group-item">Pixar</li>
@@ -115,9 +117,10 @@
           </p>
           <div class="collapse" id="collapseExample">
             <div class="card card-body">
-              <p class="card-text">Captain Jack Sparrow works his way out of a blood debt with the ghostly Davy Jones to avoid eternal damnation.</p>
+
           
             <ul class="list-group list-group-flush">
+              <li class="list-group-item">Captain Jack Sparrow works his way out of a blood debt with the ghostly Davy Jones to avoid eternal damnation.</li>
               <li class="list-group-item">Adventure / Action</li>
               <li class="list-group-item">2006</li>
               <li class="list-group-item">Disney</li>
@@ -140,9 +143,10 @@
           </p>
           <div class="collapse" id="collapseExample">
             <div class="card card-body">
-              <p class="card-text">Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.</p>
+            
           
             <ul class="list-group list-group-flush">
+              <li class="list-group-item">Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.</li>
               <li class="list-group-item">Adventure / Action</li>
               <li class="list-group-item">1978</li>
               <li class="list-group-item">Lucasfilm</li>
@@ -165,9 +169,9 @@
           </p>
           <div class="collapse" id="collapseExample">
             <div class="card card-body">
-              <p class="card-text">After Homer accidentally pollutes the town's water supply, Springfield is encased in a gigantic dome by the EPA and the Simpsons are declared fugitives.</p>
-          
+        
             <ul class="list-group list-group-flush">
+              <li class="list-group-item">After Homer accidentally pollutes the town's water supply, Springfield is encased in a gigantic dome by the EPA and the Simpsons are declared fugitives.</li>
               <li class="list-group-item">Animation / Comedy</li>
               <li class="list-group-item">2007</li>
               <li class="list-group-item">Gracie Films</li>
@@ -190,9 +194,9 @@
           </p>
           <div class="collapse" id="collapseExample">
             <div class="card card-body">
-              <p class="card-text">In the year 180, the death of emperor Marcus Aurelius throws the Roman Empire into chaos.  Maximus is one of the Roman army's most capable and trusted generals and a key advisor to the emperor.  As Marcus' devious son Commodus ascends to the throne, Maximus is set to be executed.  He escapes, but is captured by slave traders.  Renamed Spaniard and forced to become a gladiator, Maximus must battle to the death with other men for the amusement of paying audiences.</p>
           
             <ul class="list-group list-group-flush">
+              <li class="list-group-item">In the year 180, the death of emperor Marcus Aurelius throws the Roman Empire into chaos.  Maximus is one of the Roman army's most capable and trusted generals and a key advisor to the emperor.  As Marcus' devious son Commodus ascends to the throne, Maximus is set to be executed.  He escapes, but is captured by slave traders.  Renamed Spaniard and forced to become a gladiator, Maximus must battle to the death with other men for the amusement of paying audiences.</li>
               <li class="list-group-item">Action / Drama</li>
               <li class="list-group-item">2000</li>
               <li class="list-group-item">Universal Pictures</li>
@@ -215,9 +219,8 @@
           </p>
           <div class="collapse" id="collapseExample">
             <div class="card card-body">
-              <p class="card-text">In the smog-choked dystopian Los Angeles of 2019, blade runner Rick Deckard is called out of retirement to terminate a quartet of replicants who have escaped to Earth seeking their creator for a way to extend their short life spans.</p>
-          
             <ul class="list-group list-group-flush">
+              <li class="list-group-item">In the smog-choked dystopian Los Angeles of 2019, blade runner Rick Deckard is called out of retirement to terminate a quartet of replicants who have escaped to Earth seeking their creator for a way to extend their short life spans./li>
               <li class="list-group-item">Sci-Fi / Drama</li>
               <li class="list-group-item">1982</li>
               <li class="list-group-item">Warner Bros.</li>
@@ -240,9 +243,10 @@
           </p>
           <div class="collapse" id="collapseExample">
             <div class="card card-body">
-              <p class="card-text">In 1938, an art collector appeals to eminent archaeologist Dr. Indiana Jones to embark on a search for the Holy Grail. Indy learns that a medieval historian has vanished while searching for it, and the missing man is his own father, Dr. Henry Jones Sr.. He sets out to rescue his father by following clues in the old man's notebook, which his father had mailed to him before he went missing. Indy arrives in Venice, where he enlists the help of a beautiful academic, Dr. Elsa Schneider, along with Marcus Brody and Sallah. Together they must stop the Nazis from recovering the power of eternal life and taking over the world!</p>
+              
           
             <ul class="list-group list-group-flush">
+              <li class="list-group-item">In 1938, an art collector appeals to eminent archaeologist Dr. Indiana Jones to embark on a search for the Holy Grail. Indy learns that a medieval historian has vanished while searching for it, and the missing man is his own father, Dr. Henry Jones Sr.. He sets out to rescue his father by following clues in the old man's notebook, which his father had mailed to him before he went missing. Indy arrives in Venice, where he enlists the help of a beautiful academic, Dr. Elsa Schneider, along with Marcus Brody and Sallah. Together they must stop the Nazis from recovering the power of eternal life and taking over the world!</li>
               <li class="list-group-item">Adventure / Action</li>
               <li class="list-group-item">1989</li>
               <li class="list-group-item">Paramount Pictures</li>
@@ -265,9 +269,8 @@
           </p>
           <div class="collapse" id="collapseExample">
             <div class="card card-body">
-              <p class="card-text">Ray Ferrier is a divorced dockworker and less-than-perfect father. Soon after his ex-wife and her new husband drop off his teenage son and young daughter for a rare weekend visit, a strange and powerful lightning storm touches down.</p>
-          
             <ul class="list-group list-group-flush">
+              <li class="list-group-item">Ray Ferrier is a divorced dockworker and less-than-perfect father. Soon after his ex-wife and her new husband drop off his teenage son and young daughter for a rare weekend visit, a strange and powerful lightning storm touches down.</li>
               <li class="list-group-item">Adventure / Thriller</li>
               <li class="list-group-item">2005</li>
               <li class="list-group-item">Paramount Pictures</li>
@@ -290,9 +293,8 @@
           </p>
           <div class="collapse" id="collapseExample">
             <div class="card card-body">
-              <p class="card-text">Leonard Shelby is tracking down the man who raped and murdered his wife. The difficulty of locating his wife's killer, however, is compounded by the fact that he suffers from a rare, untreatable form of short-term memory loss. Although he can recall details of life before his accident, Leonard cannot remember what happened fifteen minutes ago, where he's going, or why.</p>
-          
               <ul class="list-group list-group-flush">
+                <li class="list-group-item">Leonard Shelby is tracking down the man who raped and murdered his wife. The difficulty of locating his wife's killer, however, is compounded by the fact that he suffers from a rare, untreatable form of short-term memory loss. Although he can recall details of life before his accident, Leonard cannot remember what happened fifteen minutes ago, where he's going, or why.</li>
                 <li class="list-group-item">Mystery / Thriller</li>
                 <li class="list-group-item">2001</li>
                 <li class="list-group-item">Summit Entertainment</li>
@@ -315,9 +317,9 @@
           </p>
           <div class="collapse" id="collapseExample">
             <div class="card card-body">
-              <p class="card-text">Expecting the usual tedium that accompanies a summer in the Catskills with her family, 17-year-old Frances 'Baby' Houseman is surprised to find herself stepping into the shoes of a professional hoofer—and unexpectedly falling in love.</p>
           
             <ul class="list-group list-group-flush">
+              <li class="list-group-item">Expecting the usual tedium that accompanies a summer in the Catskills with her family, 17-year-old Frances 'Baby' Houseman is surprised to find herself stepping into the shoes of a professional hoofer—and unexpectedly falling in love.</li>
               <li class="list-group-item">Drama / Music</li>
               <li class="list-group-item">1987</li>
               <li class="list-group-item">Great American Films Limited Partnership</li>
