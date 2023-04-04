66f2cfa6b2d9934f33d3f0f6370e6233c0ecffdf
Max24p7 <82169517+Max24p7@users.noreply.github.com>
2023-04-04 15:35:15 -0500

    Button Fix

--- movies.php
+++ movies.php
@@ -5,7 +5,6 @@ $page_title = 'Movies';?>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Movies | Bonus Vision</title>
 
   <style>
     .card-img-top {
