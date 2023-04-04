4112f9393e810de78af89f1a9871da96755e055e
TheKooperDon <valodinejr@gmail.com>
2023-04-04 09:16:12 -0500

    Update movies.php

--- movies.php
+++ movies.php
@@ -13,6 +13,8 @@
     .card-img-top {
         border-radius: 10px;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        margin: 0 auto;
+        padding: 0px;
     }
     .btn btn-primary {
       border: black 1px;
